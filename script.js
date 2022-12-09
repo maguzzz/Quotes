c1f25567846f2edf57e802ed46da1a2d42414073
maguzzz <markusm.touray@gmail.com>
2022-12-09 14:06:59 +0100

    getting api data

---
+++ script.js
@@ -0,0 +1,15 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e182d12ebemsh85f8f05c8d8eb03p14cc5djsn44b0baed8501',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+	.then(response => response.json())
+	.then(data =>{
+		document.getElementById("quote").innerHTML = data.content;
+		document.getElementById("quoteAuthor").innerHTML = `-${data.originator.name}`;
+	})
+	.catch(err => console.error(err));
